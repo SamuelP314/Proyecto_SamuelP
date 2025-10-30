f280160618ecf87d4f088eeaf78a7d28c91c57d6
Clase <200045@iessanalberto.com>
2025-10-30 13:27:47 +0100

    Commit13

--- scripts/test.js
+++ scripts/test.js
@@ -1,22 +1,42 @@
 function comprobar(){
     event.preventDefault()
     rdRespuestasP4 = document.getElementsByName("P4")
-    txtCheck = document.getElementById("txtCheck")
-    respuestaOK=false
+    respuesta4OK=false
     for (radio of rdRespuestasP4){
         if (radio.checked){
-            txtCheck.innerHTML = "Respuesta: " + radio.value
             if (radio.value == "ARPANET"){
-                respuestaOK = true
+                respuesta4OK = true
             }
         }
-}
+    }
 
-if (respuestaOK){
-    document.getElementById("imgP4").src="media/BienImg.png"
-}else{
-    document.getElementById("imgP4").src="media/MalImg.png"
-}
-document.getElementById("imgP4").style.visibility="visible"
+    if (respuesta4OK){
+        document.getElementById("imgP4").src="../media/BienImg.jpg"
+    }else{
+        document.getElementById("imgP4").src="../media/MalImg.jpg"
+    }
+    document.getElementById("imgP4").style.visibility="visible"
 
-}
+}  
+
+function comprobar(){
+    event.preventDefault()
+    rdRespuestasP6 = document.getElementsByName("P6")
+    respuesta6OK=false
+    for (radio of rdRespuestasP6){
+        if (radio.checked){
+            if (radio.value == "Años 2000"){
+                respuesta6OK = true
+            }
+        }
+    }
+
+    if (respuesta6OK){
+        document.getElementById("imgP6").src="../media/BienImg.jpg"
+    }else{
+        document.getElementById("imgP6").src="../media/MalImg.jpg"
+    }
+    document.getElementById("imgP6").style.visibility="visible"
+
+}  
+
