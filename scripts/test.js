7353ed2a74dbd8c9862438414f0823d4273380cf
Clase <200045@iessanalberto.com>
2025-10-31 09:19:19 +0100

    Commit14B

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,9 @@
 function comprobar(){
+
+
+
+
+
     event.preventDefault()
     rdRespuestasP4 = document.getElementsByName("P4")
     respuesta4OK=false
@@ -17,15 +22,16 @@ function comprobar(){
     }
     document.getElementById("imgP4").style.visibility="visible"
 
-}  
 
-function comprobar(){
+
+
+
     event.preventDefault()
     rdRespuestasP6 = document.getElementsByName("P6")
     respuesta6OK=false
     for (radio of rdRespuestasP6){
         if (radio.checked){
-            if (radio.value == "Años 2000"){
+            if (radio.value == "2000"){
                 respuesta6OK = true
             }
         }
@@ -38,5 +44,46 @@ function comprobar(){
     }
     document.getElementById("imgP6").style.visibility="visible"
 
-}  
 
+
+
+
+    event.preventDefault()
+    rdRespuestasP8 = document.getElementsByName("P8")
+    respuesta8OK=false
+    for (radio of rdRespuestasP8){
+        if (radio.checked){
+            if (radio.value == "CSS"){
+                respuesta8OK = true
+            }
+        }
+    }
+
+    if (respuesta8OK){
+        document.getElementById("imgP8").src="../media/BienImg.jpg"
+    }else{
+        document.getElementById("imgP8").src="../media/MalImg.jpg"
+    }
+    document.getElementById("imgP8").style.visibility="visible"
+
+
+
+
+    event.preventDefault()
+    rdRespuestasP11 = document.getElementsByName("P11")
+    respuesta11OK=false
+    for (radio of rdRespuestasP11){
+        if (radio.checked){
+            if (radio.value == "Web3.0"){
+                respuesta11OK = true
+            }
+        }
+    }
+
+    if (respuesta11OK){
+        document.getElementById("imgP11").src="../media/BienImg.jpg"
+    }else{
+        document.getElementById("imgP11").src="../media/MalImg.jpg"
+    }
+    document.getElementById("imgP11").style.visibility="visible"
+}  
